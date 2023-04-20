4826c056dd43c73818b130bc40a6d249642e5930
CaurissaN <caurissanev@gmail.com>
2023-04-20 11:51:38 -0400

    Updated the code to create a password

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 function generatePassword() {
+  // this variable holds the length of password
   var lengthOfPassword = prompt("How long would you like your password to be? (8-128)")
   if(lengthOfPassword < 8) {
     alert("bad input");
@@ -21,20 +21,41 @@ function generatePassword() {
     alert("bad input");
     return "bad input, error, or else"
   }
+
   var passwordCriteria= "";
+  var answer = ""
 
   var lowercase = confirm ("Do you want lower in your password?");
   if(lowercase === true) {
     passwordCriteria += "abcdefghijklmnopqrstuvwxyz"
   }
-
   var uppercase = confirm ("Do you want uppercase in your password?");
   if (uppercase ===true){
     passwordCriteria += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   }
+  var numbers = confirm ("Do you want numbers in your password?");
+  if(numbers === true) {
+    passwordCriteria += "1234567890"
+  }
+  var symbols = confirm ("Do you want symbols in your password?");
+  if(symbols === true) {
+    passwordCriteria += "!@#$%^&*()_"
+  }
+
+  if(passwordCriteria.length == 0) {
+    alert("You must choose atleast one type of character, to create a password.")
+    return "You must choose atleast one type of character, to create a password."
+  }
+
+  for(let i = 0; i < lengthOfPassword; i++) {
+    var random = Math.floor(Math.random() * passwordCriteria.length)
+    answer += passwordCriteria[random]
+  }
+
+
 
   // use confirm instead of prompt for acceptance criteria about (lower case, upper case, etc)
-  return passwordCriteria
+  return answer
 }
 
 // Add event listener to generate button
